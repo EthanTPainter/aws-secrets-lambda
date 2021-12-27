0d65618a1136998b89d88a9c87e30bc68560be8c
Ethan Painter <ethantpainter@gmail.com>
2021-12-27 15:54:26 -0500

    Update to include cfnresponse import and remove debug print lines

--- lambda/app.py
+++ lambda/app.py
@@ -1,12 +1,21 @@
 import boto3
+import cfnresponse
 import json
-import requests
 
 def secrets_handler(event, context):
   ssmClient = boto3.client("ssm")
-  print(event)
-  value = ssmClient.get_parameter(Name=event["SSMKey"], WithDecryption=True)
+  # Retrieving from resource properties since this is the format
+  # from a custom resource in a CloudFormation template
+  requestedSSMParameter = event["ResourceProperties"]["SSMKey"]
+  value = ssmClient.get_parameter(Name=requestedSSMParameter, WithDecryption=True)
 
+  # Raw Data for CFN Response
+  cfnRespData = {
+    "Value": value["Parameter"]["Value"]
+  }
+  cfnresponse.send(event, context, cfnresponse.SUCCESS, cfnRespData)
+
+  # Data to return if avoiding the CFN response
   data = json.dumps({
     "Value": value["Parameter"]["Value"]
   })
