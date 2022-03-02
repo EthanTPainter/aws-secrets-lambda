9fed2554bab4c62d833c27a35361607f2a32fe8a
Ethan Painter <ethantpainter@gmail.com>
2022-03-01 23:00:54 -0500

    add try catch for api error

--- lambda/app.py
+++ lambda/app.py
@@ -7,7 +7,25 @@ def secrets_handler(event, context):
   # Retrieving from resource properties since this is the format
   # from a custom resource in a CloudFormation template
   requestedSSMParameter = event["ResourceProperties"]["SSMKey"]
-  value = ssmClient.get_parameter(Name=requestedSSMParameter, WithDecryption=True)
+  try:
+    value = ssmClient.get_parameter(Name=requestedSSMParameter, WithDecryption=True)
+  except:
+    badCfnResponseData = {
+      "Value": "bad value"
+    }
+    cfnresponse.send(event, context, cfnresponse.FAILED, badCfnResponseData)
+    return json.dumps({
+      "Value": "bad value"
+    })
+
+  if (value is None or value["Parameter"] is None or value["Parameter"]["Value"] is None):
+    badCfnResponseData = {
+      "Value": "bad value"
+    }
+    cfnresponse.send(event, context, cfnresponse.FAILED, badCfnResponseData)
+    return json.dumps({
+      "Value": "bad value"
+    })
 
   # Raw Data for CFN Response
   cfnRespData = {
